12b9fa6f4ae13fa5eb28ca3df0cca3d94c75eccc
philip713 <105677806+philip713@users.noreply.github.com>
2024-09-12 23:36:20 -0600

    Initial Commit

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -15,8 +15,26 @@ struct ContentView: View {
     @State private var showingAddScreen = false
     var body: some View {
         NavigationStack{
-            Text("Count: \(books.count)")
+            List{
+                ForEach(books){ book in
+                    NavigationLink(value: book){
+                        HStack{
+                            EmojiRatingView(rating: book.rating)
+                                .font(.largeTitle)
+                            VStack(alignment: .leading){
+                                Text(book.title)
+                                    .font(.headline)
+                                Text(book.author)
+                                    .foregroundStyle(.secondary)
+                            }
+                        }
+                    }
+                }
+            }
                 .navigationTitle("Bookworm")
+                .navigationDestination(for: Book.self){ book in
+                    DetailView(book: book)
+                }
                 .toolbar{
                     ToolbarItem(placement: .topBarTrailing){
                         Button("Add Book", systemImage: "plus"){
---
+++ Bookworm/EmojiRatingView.swift
@@ -0,0 +1,30 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Philip Janzel Paradeza on 2024-12-08.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating: Int
+    var body: some View {
+        switch rating{
+        case 1:
+            Text("1")
+        case 2:
+            Text("2")
+        case 3:
+            Text("3")
+        case 4:
+            Text("4")
+        default:
+            Text("5")
+        }
+    }
+}
+
+#Preview {
+    EmojiRatingView(rating: 3)
+}
