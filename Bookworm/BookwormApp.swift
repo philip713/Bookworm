19e49cde0fa60276def230503f2ce07cc9637a9b
philip713 <105677806+philip713@users.noreply.github.com>
2024-09-12 23:36:20 -0600

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Philip Janzel Paradeza on 2024-09-12.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
